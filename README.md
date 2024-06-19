c068eac15fd2f6e69420b1ffbbd094079d2bc3b1
Alejandro Reyes <reyesbalejandroe@gmail.com>
2024-06-19 13:45:33 +0200

    update/README

--- README.md
+++ README.md
@@ -1,1 +1,67 @@
-# demo_jwt
+# API REST + JWT
+
+API REST of user login, logout and view a list of users with with Node.js, Express using jwt(jsonwebtoken) authentication
+
+
+## Libraries
+
+- Express
+- Jsonwebtoken
+
+## Installation
+
+Install dependencies
+```javascript
+npm i 
+```
+
+Start server in localhost:3000
+```javascript
+node index.js
+```
+
+## Endpoints
+
+- [GET] http://localhost:3000/ Hello world route
+
+- [POST] http://localhost:3000/autenticar Login user. Returns JWT of the user
+
+```json
+{
+  "usuario":"alex",
+  "contrasena":"123456"
+} 
+```
+Payload. Example JWT returned
+```json
+{
+  "mensaje": "Autenticación correcta",
+  "token": "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJjaGVjayI6dHJ1ZSwidXNlciI6ImFsZXgiLCJwYXNzd29yZF9iYW5jbyI6IjEyMzQ1NiIsImlhdCI6MTcxODc5NzE1NywiZXhwIjoxNzE4Nzk3MjE3fQ.sYyE7HWQVScnHmuX3h3RwBhcibdeXmgnmsCQwPW0fHY"
+}
+```
+
+### Get all users - protected route
+- [GET] http://localhost:3000/datos Get all users if we pass a valid JWT in HTTP ` access-token ` header. HTTP Header: 
+
+**HTTP header**: access-token
+
+**Value**: eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VybmFtZSI6ImFubmEiLCJyb2xlIjoibWVtYmVyIiwiaWF0IjoxNjk5OTU2ODMyLCJleHAiOjE2OTk5NTgwMzJ9.CpnJGtPa5h6gjKjlHtcC4aFtdhf8e8bvFK_eXE5DZEE
+
+HTTP response
+
+```json
+[
+  {
+    "id": 1,
+    "nombre": "Birja"
+  },
+  {
+    "id": 2,
+    "nombre": "Muchelle"
+  },
+  {
+    "id": 3,
+    "nombre": "Alvaru"
+  }
+]
+```
--- index.js
+++ index.js
@@ -1,15 +1,15 @@
-const express = require('express'),
-      jwt = require('jsonwebtoken'),
-      app = express(); 
-	  
+const express = require("express"),
+  jwt = require("jsonwebtoken"),
+  app = express();
+
 // Clave privada para firmar los Tokens
 // Guardar la clave en la BBDD
-const config = { 
-	llave : "miclaveultrasecreta123*"
+const config = {
+  llave: "miclaveultrasecreta123*",
 };
 
 // 1
-app.set('llave', config.llave);
+app.set("llave", config.llave);
 
 // 2
 app.use(express.urlencoded({ extended: true }));
@@ -17,62 +17,72 @@ app.use(express.urlencoded({ extended: true }));
 // 3
 app.use(express.json());
 
-app.listen(3000,()=>{
-    console.log('Servidor iniciado en el puerto 3000') 
+app.listen(3000, () => {
+  console.log("Servidor iniciado en el puerto 3000");
 });
 
 // 4
-app.get('/', function(req, res) {
-    res.json({ message: 'recurso de entrada' });
+app.get("/", function (req, res) {
+  res.json({ message: "recurso de entrada" });
 });
 
 // 5
-app.post('/autenticar', (req, res) => {
-    if(req.body.usuario === "alex" && req.body.contrasena === "123456") {
-		const payload = {
-			check:  true,
-            user:"alex"
-		};
-		const token = jwt.sign(payload, app.get('llave'), {
-			expiresIn: "30000ms" // 30 segundos para que expire
-		});
-		res.json({
-			mensaje: 'Autenticación correcta',
-			token: token
-		});
-    } else {
-        res.json({ mensaje: "Usuario o contraseña incorrectos"})
-    }
-})
+/* 
+POST /autenticar
+{
+  "usuario":"alex",
+  "contrasena":"123456"
+} 
+*/
+app.post("/autenticar", (req, res) => {
+  if (req.body.usuario === "alex" && req.body.contrasena === "123456") {
+    const payload = {
+      check: true,
+      user: "alex",
+      password_banco: "123456",
+    };
+    const token = jwt.sign(payload, app.get("llave"), {
+      expiresIn: "60000ms", // 30 segundos para que expire
+    });
+    res.json({
+      mensaje: "Autenticación correcta",
+      token: token,
+    });
+  } else {
+    res.json({ mensaje: "Usuario o contraseña incorrectos" });
+  }
+});
 
 // 6
-const rutasProtegidas = express.Router(); 
+// Middleware para proteger la ruta /datos
+// Rellenar cabecera access-token en la petición de postman
+const rutasProtegidas = express.Router();
 rutasProtegidas.use((req, res, next) => {
-    const token = req.headers['access-token'];
-	
-    if (token) {
-      jwt.verify(token, app.get('llave'), (err, decoded) => {      
-        if (err) {
-          return res.json({ mensaje: 'Token inválida' });    
-        } else {
-          req.decoded = decoded;    
-          next();
-        }
-      });
-    } else {
-      res.send({ 
-          mensaje: 'Token no proveída.' 
-      });
-    }
- });
+  const token = req.headers["access-token"];
 
-app.get('/datos', rutasProtegidas, (req, res) => {
-    // Consulta a la "BBDD"
-	const datos = [
-		{ id: 1, nombre: "Birja" },
-		{ id: 2, nombre: "Muchelle" },
-		{ id: 3, nombre: "Alvaru" }
-	];
-	
-	res.json(datos);
+  if (token) {
+    jwt.verify(token, app.get("llave"), (err, decoded) => {
+      if (err) {
+        return res.json({ mensaje: "Token inválida" });
+      } else {
+        req.decoded = decoded;
+        next();
+      }
+    });
+  } else {
+    res.send({
+      mensaje: "Token no proveída.",
+    });
+  }
+});
+
+app.get("/datos", rutasProtegidas, (req, res) => {
+  // Consulta a la "BBDD"
+  const datos = [
+    { id: 1, nombre: "Birja" },
+    { id: 2, nombre: "Muchelle" },
+    { id: 3, nombre: "Alvaru" },
+  ];
+
+  res.json(datos);
 });
